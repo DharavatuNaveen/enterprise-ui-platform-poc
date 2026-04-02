553c564f27ba6f26af7c7a1f7ab571ef8fe148d8
DharavatuNaveen <naveen.d@pravaltech.com>
2026-04-02 21:30:41 +0530

    package setup

--- poc/vite.config.ts
+++ poc/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 
+// export default defineConfig({
+//   base: '/enterprise-ui-platform-poc/',
+//   build: {
+//     outDir: 'docs',
+//   },
+// });
+
+
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/enterprise-ui-platform-poc/',
-  build: {
-    outDir: 'docs',
-  },
-});
+  base: '/enterprise-ui-platform-poc/'
+})
